bce2316d29c560ed757c0154f6f5c6d7325de125
Monika Sharma <monikasharma279826@gmail.com>
2025-05-25 12:45:43 +0530

    done

--- Frontend/axios.js
+++ Frontend/axios.js
@@ -1,12 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   headers: {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json",
   },
-  withCredentials: true
+  withCredentials: true,
 });
 
-export default axiosInstance  
+export default axiosInstance;
