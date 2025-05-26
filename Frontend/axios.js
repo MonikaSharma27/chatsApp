a69ebfc962bbe1e4a36806098f350e56092f5663
aadesh-cisit <aadesh.kumar@cisit.in>
2025-05-27 00:36:35 +0530

    for prod

--- Frontend/axios.js
+++ Frontend/axios.js
@@ -3,12 +3,42 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
   headers: {
     "Content-Type": "application/json",
+    "Accept": "application/json"
   },
   withCredentials: true,
+  // Remove methods and allowedHeaders as they are not valid axios config options
 });
+
+// Add request interceptor to handle errors
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // You can add any request modifications here
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Add response interceptor to handle errors
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error('Response Error:', error.response.data);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error('Request Error:', error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.error('Error:', error.message);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
