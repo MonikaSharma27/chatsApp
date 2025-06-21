2bf9b8979ae3e16872b21e240c7951f3201a871a
Monika Sharma <monikasharma279826@gmail.com>
2025-06-21 19:45:08 +0530

    axiosinstance changes

--- Frontend/axios.js
+++ Frontend/axios.js
@@ -11,34 +11,8 @@ const axiosInstance = axios.create({
   // Remove methods and allowedHeaders as they are not valid axios config options
 });
 
-// Add request interceptor to handle errors
-axiosInstance.interceptors.request.use(
-  (config) => {
-    // You can add any request modifications here
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
-// Add response interceptor to handle errors
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.error('Response Error:', error.response.data);
-    } else if (error.request) {
-      // The request was made but no response was received
-      console.error('Request Error:', error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.error('Error:', error.message);
-    }
-    return Promise.reject(error);
-  }
-);
+
+
 
 export default axiosInstance;
